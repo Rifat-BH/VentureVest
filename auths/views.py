869dc31df7571dcba97edf1883ea521f26661bbb
sayemsiam <sayemsiam198@gmail.com>
2023-03-25 03:01:34 +0600

    auth base

--- auths/views.py
+++ auths/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def login(request):
+    # return HttpResponse("Hi")
+    return render(request, "login.html")
