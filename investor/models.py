d0ac7c24407c5681feb88c79741ea04f55412383
sayemsiam <sayemsiam198@gmail.com>
2023-03-31 17:26:21 +0600

    investor v1.5

--- investor/models.py
+++ investor/models.py
@@ -4,8 +4,10 @@ from auths.models import Auts
 # Create your models here.
 class Invest(models.Model):
     user_id = models.ForeignKey(Auts,blank = True, null = True, on_delete= models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     company_name = models.CharField(max_length=50)
     invest_ammount = models.CharField(max_length=10)
     returen_rate = models.CharField(max_length=5)
+
     
+# auto_now_add=True
--- investor/views.py
+++ investor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from auths.models import Auts
 from investor.models import Invest
-from django.db.models import Count
+from django.db.models import Count,Sum,Avg
 from django.db.models.functions import TruncMonth
 # Create your views here.
 def home(request):
@@ -12,7 +12,7 @@ def home(request):
     if request.method == 'GET':
         i_id = request.GET.get('id')
         user_des = Auts.objects.get(id=i_id)
-        inv_data = Invest.objects.filter(user_id = i_id).values()
+        inv_data = Invest.objects.filter(user_id = i_id).annotate(co=Count('user_id')).values("co").annotate(total_am=Sum('invest_ammount'), avg_ret=Avg('returen_rate')).values('co', 'total_am', 'avg_ret')
         print(inv_data)
     data={
         'name' : user_des,
@@ -26,3 +26,6 @@ def get_data_table(request):
     return JsonResponse({"t_data" :list(inv_data.values())})
 def get_data_graph1(request):
     i_id = request.GET.get('id')
+
+    g_data = Invest.objects.filter(user_id = i_id).annotate(month=TruncMonth('date')).values('month').annotate(total=Sum('invest_ammount')).values('month','total') 
+    return JsonResponse({'g_data' : list(g_data)})
