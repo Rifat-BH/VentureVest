cb1f53d2a862d9c63701e86c706ee01570673942
sayemsiam <sayemsiam198@gmail.com>
2023-03-31 23:51:12 +0600

    v2

--- auths/urls.py
+++ auths/urls.py
@@ -4,10 +4,7 @@ from auths import views
 
 urlpatterns = [
     path('', views.login, name="login"),
-<<<<<<< HEAD
-=======
     path('login/', views.login, name="login"),
->>>>>>> startup
     path('signup/', views.signup, name="signup"),
     path('signout/', views.logout, name="logout"),
 ]
