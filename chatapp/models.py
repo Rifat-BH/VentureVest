92921f51f2c30870cb0fa91affce0b07f7ce4663
sayemsiam <sayemsiam198@gmail.com>
2023-04-22 13:14:45 +0600

    initial commit community

---
+++ chatapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from auths.models import Auts
+from datetime import datetime
+
+class MessageDb(models.Model):
+    s_id = models.ForeignKey(Auts,blank = True, null = True, on_delete= models.CASCADE,related_name='s_id')
+    r_id = models.ForeignKey(Auts,blank = True, null = True, on_delete= models.CASCADE, related_name='r_id')
+    send_date = models.DateTimeField(default=datetime.now())
+    msgg = models.CharField(max_length=1000)
+    status = models.CharField(max_length=7, default ="unseen")
+
+
--- investor/views.py
+++ investor/views.py
@@ -8,24 +8,26 @@ from django.db.models.functions import TruncMonth
 def home(request):
     # profile = Auts.objects.filter(id = investor_id).values()
     # print(profile)
-    
-    if request.method == 'GET':
-        i_id = request.GET.get('id')
-        user_des = Auts.objects.get(id=i_id)
-        inv_data = Invest.objects.filter(user_id = i_id).annotate(co=Count('user_id')).values("co").annotate(total_am=Sum('invest_ammount'), avg_ret=Avg('returen_rate')).values('co', 'total_am', 'avg_ret')
-        print(inv_data)
+    id = request.session['id']
+    print(id)
+
+    # i_id = request.GET.get('id')
+    user_des = Auts.objects.get(id=id)
+    inv_data = Invest.objects.filter(user_id = id).annotate(co=Count('user_id')).values("co").annotate(total_am=Sum('invest_ammount'), avg_ret=Avg('returen_rate')).values('co', 'total_am', 'avg_ret')
+    print(inv_data)
     data={
         'name' : user_des,
         'inv_data' : inv_data
     }
     return render(request,"investor.html", data)
 def get_data_table(request):
-    i_id = request.GET.get('id')
-    inv_data = Invest.objects.filter(user_id = i_id)
+    # i_id = request.GET.get('id')
+    id = request.session['id']
+    inv_data = Invest.objects.filter(user_id = id)
 
     return JsonResponse({"t_data" :list(inv_data.values())})
 def get_data_graph1(request):
-    i_id = request.GET.get('id')
-
-    g_data = Invest.objects.filter(user_id = i_id).annotate(month=TruncMonth('date')).values('month').annotate(total=Sum('invest_ammount')).values('month','total') 
+    # i_id = request.GET.get('id')
+    id = request.session['id']
+    g_data = Invest.objects.filter(user_id = id).annotate(month=TruncMonth('date')).values('month').annotate(total=Sum('invest_ammount')).values('month','total') 
     return JsonResponse({'g_data' : list(g_data)})
