5972a9301edeebde65f6c3be938de22e447186a1
RifatBH <rsarifat@gmail.com>
2023-04-26 12:26:37 +0600

    admin details work done. approval and deny work not complete

--- adminControl/views.py
+++ adminControl/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from auths.models import Auts
+from backupStartupDB.models import startupBasicInfo2, applyForFundrising, monthlyRevenue
+
 
 # Create your views here.
 def adminApproval(request):
-    
-     return render(request,"admin.html")
+    approve = applyForFundrising.objects.filter(status = 0)
+    data={
+        'approve':approve
+    }
+    return render(request,"admin.html",data)
